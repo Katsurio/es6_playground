5fa3ba8658d389718e7b5074880c1959faf9e2d5
John (Jef) Fisher <Fishe545@gmail.com>
2017-08-17 21:22:23 -0700

    repeat_string is finished

--- basic_algorithm_scripts/confirm_the_end/main.js
+++ basic_algorithm_scripts/confirm_the_end/main.js
@@ -4,14 +4,18 @@ This challenge can be solved with the .endsWith() method, which was introduced i
 But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
 */
 
-function confirmEnding(str, target) {
-    let splitStr = str.split(' ');
-    let lengthMinus1 = (splitStr.length) - 1;
-    let word = splitStr.length > 1 ? splitStr[lengthMinus1] : str;
-    return (word.substr(-target.length) === target);
-}
+// function confirmEnding(str, target) {
+//     let splitStr = str.split(' ');
+//     let lengthMinus1 = (splitStr.length) - 1;
+//     let word = splitStr.length > 1 ? splitStr[lengthMinus1] : str;
+//     return (word.substr(-target.length) === target);
+// }
 
-console.log(confirmEnding("Connor", "n"));
+// function confirmEnding(str, target) {
+//     return (str.substr(-target.length) === target);
+// }
+//
+// console.log(confirmEnding("Connor", "n"));
 
 /*
 function with 2 prams (str, match)
